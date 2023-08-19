cf55ac39ff732f6db02cb6897550b4207e352841
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-08-20 02:35:57 +0600

    get input value and set element inner text function added

---
+++ Js/index.js
@@ -0,0 +1,12 @@
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
