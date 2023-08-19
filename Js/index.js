620daf43f1b4e8f6f90fea8f92ccd204f4092aad
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-08-20 03:42:31 +0600

    card added in cart function setting complete

--- Js/index.js
+++ Js/index.js
@@ -1,12 +1,30 @@
 
 function getInputValue(fieldId){
     const inputField = document.getElementById(fieldId);
-    const inputValueText = inputField.value;
-    const value = parseFloat(inputValueText);
-    return value;
+    const inputValueText = inputField.innerText;
+    return inputValueText;
 }
 
-function setElementInnerText(elementId, area){
-    const element = document.getElementById(elementId);
-    element.innerText = area;
+function addCard1(){
+    const card1Name = getInputValue('card-1-name');
+    const card1Price = getInputValue('card-1-price');
+
+    const cartListedItems = document.getElementById('added-product');
+    const count = cartListedItems.childElementCount;
+    const p = document.createElement('p');
+    p.innerHTML = `${count+1}`+'.'+ '  ' +card1Name;
+    cartListedItems.appendChild(p);
+    const totalPrice = document.getElementById('total-price');
+    
+    const totalPriceAmount = totalPrice.innerText + card1Price;
+    console.log(totalPriceAmount)
+}
+function addCard2(){
+    const card2Name = getInputValue('card-2-name');
+
+    const cartListedItems = document.getElementById('added-product');
+    const count = cartListedItems.childElementCount;
+    const p = document.createElement('p');
+    p.innerHTML = `${count+1}`+'.'+ '  ' +card2Name;
+    cartListedItems.appendChild(p);
 }
